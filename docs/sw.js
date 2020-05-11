afceea3f31aaf5aa190ef5deebe4c478e555aeec
unknown <tomomoichok@gmail.com>
2020-05-11 15:41:01 +0900

    init

--- docs/sw.js
+++ docs/sw.js
@@ -1,31 +1,9 @@
-// ServiceWorker処理：https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja
-
-// キャッシュ名とキャッシュファイルの指定
-var CACHE_NAME = 'pwa-sample-caches';
-var urlsToCache = [
-	'/pwatest/',
-	'/pwatest/css/style.css',
-	// '/pwa/drawer.js'
-];
-
-// インストール処理
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
-    );
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
 });
 
-// リソースフェッチ時のキャッシュロード処理
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(response) {
-                return response ? response : fetch(event.request);
-            })
-    );
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
 });
+
+self.addEventListener('fetch', function(event) {});
